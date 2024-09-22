5b763c505b2fb50d0c864bc5db6ae6906868ebf6
edayigit <yigit.eda@student.atilim.edu.tr>
2024-09-23 02:09:50 +0300

    İlk commit

---
+++ DBContext/PointContext.cs
@@ -0,0 +1,19 @@
+﻿using MapApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+
+namespace MapApp.DBContext
+{
+    public class PointContext : DbContext
+    {
+        public PointContext(DbContextOptions options) : base(options)
+        {
+            //options ile bağlantı dizesi alınmış olacak
+        }
+        public DbSet<Point> Points { get; set; }
+
+    }
+}
+
